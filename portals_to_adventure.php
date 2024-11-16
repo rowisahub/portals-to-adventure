71f2caa835da1cfddede0443c3fe4917d6c45118
rowisahub <rowisabeast@gmail.com>
2024-11-16 00:53:59 +0000

    Initial commit

---
+++ portals_to_adventure.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Portals to Adventure
+Description: PTA Plugin for custom submissions and voting.
+Version: 1.5.0
+Author: <a href="https://rowisabeast.com/">Rowan Wachtler</a>
+*/
+
+/* Prevent direct access */
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+/* Constants */
+define('PTA_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
---
+++ readme.txt
@@ -0,0 +1,11 @@
+===  Portals To Adventure ===
+Contributors: Rowan W and Braedon S
+Requires at least: 5.0
+Tested up to: 6.6
+Requires PHP: 7.0
+
+== Description ==
+A user submission based voting plugin
+
+== Installation ==
+2. Activate the plugin through the 'Plugins' menu in WordPress.
