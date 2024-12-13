ae63e4939f9c44a1d9c7bf2f36dd5db41c9062ec
rowisahub <rowisabeast@gmail.com>
2024-12-13 04:13:10 +0000

    Remove debug logging for status filter and submissions in admin settings

--- src/plugin/admin/admin_settings.php
+++ src/plugin/admin/admin_settings.php
@@ -416,8 +416,8 @@ class admin_settings extends Client
             $submissions = $this->submission_functions->get_all_submissions_by_state(state: $status_filter);
         }
 
-        $this->logger->debug('Status filter: ' . $status_filter);
-        $this->logger->debug('Submissions: ' . print_r($submissions, true));
+        // $this->logger->debug('Status filter: ' . $status_filter);
+        // $this->logger->debug('Submissions: ' . print_r($submissions, true));
 
         ?>
         <div class="wrap">
