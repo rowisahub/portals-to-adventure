641012b017505dbd0b5ab797ff725834ed5959cb
rowisahub <rowisabeast@gmail.com>
2025-01-23 00:37:28 +0000

    Remove conditional hiding of form container for non-admin users on submissions page

--- FrontEnd/public/JS/View_Submissions_Page.js
+++ FrontEnd/public/JS/View_Submissions_Page.js
@@ -71,12 +71,6 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     // add the message to 'entry-content' as the first child
     document.getElementsByClassName('entry-content')[0].insertBefore(contestOverMessage, document.getElementsByClassName('entry-content')[0].firstChild);
-
-    if(!pta_api_data.user_admin){
-      document.getElementsByClassName('form-container')[0].classList.add('hide');
-
-      return;
-    }
   }
 
   ifAdmin = pta_api_data.user_admin;
