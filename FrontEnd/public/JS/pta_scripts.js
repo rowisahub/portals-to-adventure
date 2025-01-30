2c2e8cfd95035f581e076c8028eb76bb496bebf9
rowisahub <rowisabeast@gmail.com>
2025-01-30 09:19:28 +0000

    Update CSS paths and add dynamic content to data bar

---
+++ FrontEnd/public/JS/pta_scripts.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const dataBar = document.getElementsByClassName('pta-data-bar')[0];
+
+  var dataBarContent = document.createElement('p');
+  dataBarContent.innerHTML = 'Data Bar Content';
+  dataBar.appendChild(dataBarContent);
+
+  //  Submissions Start Date = February 20, 2025 / Voting Start Date = February 27, 2025
+
+  // Change the innerHTML of the dataBarContent every 5 seconds
+  var count = 0;
+  var dataBarContentArray = [
+    'Submissions Start Date | February 20, 2025',
+    'Voting Start Date | February 27, 2025'
+  ];
+
+  setInterval(function() {
+    dataBarContent.innerHTML = dataBarContentArray[count];
+    count++;
+    if (count >= dataBarContentArray.length) {
+      count = 0;
+    }
+  }, 5000);
+
+
+});
